fad54d52b459710645be1d63986f645746565de0
yuzu-r <tkaneshiro@gmail.com>
2018-02-03 08:26:13 +0000

    remove report command, change from embed to normal message format

--- maigo.rb
+++ maigo.rb
@@ -7,31 +7,6 @@ prefix = ENV['DISCORD_PREFIX']
 bot = Discordrb::Commands::CommandBot.new token: ENV['DISCORD_TOKEN'], 
 																					client_id: ENV['DISCORD_CLIENT_ID'], 
 																					prefix: prefix
-def format_embed(search_term, gym) 
-	embed = Discordrb::Webhooks::Embed.new 
-	if gym['name']
-		embed.color = 0x1EFFBC
-		if gym['gmap']
-			embed.title = gym['name'] + ' (click for google map)'	
-			embed.url = gym['gmap']
-		else
-			embed.title = gym['name']
-			embed.url = nil
-		end
-		# echo back an aka if name is different from search term
-		if gym['name'].downcase != search_term.downcase
-			embed.title = search_term + ', aka ' + embed.title
-		end
-
-		embed.description = gym['address']
-
-		if gym['landmark']
-			embed.description = embed.description + "\n" + gym['landmark']
-		end
-	end
-
-	return embed
-end
 
 usage_text = prefix  + 'whereis [gym name]'
 
@@ -39,8 +14,15 @@ bot.command(:whereis, min_args: 1, description: 'find a PoGo gym', usage: usage_
 	search_term = gym.join(' ')
 	message = lookup(search_term)
 	if message['name']
-		embed_gym = 'hi' + format_embed(search_term, message)
-		bot.send_message(event.channel.id,'',false, embed_gym)
+		if message['name'].downcase != search_term.downcase
+			title = search_term + ', aka ' + message['name']
+		else
+			title = message['name']
+		end
+		event << title
+		event << message['address']
+		event << message['landmark']
+		event << message['gmap']
 	else
 		# either multiple gyms returned, or no gyms found
 		message
@@ -54,6 +36,11 @@ bot.command(:help, description: 'maigo-helper help') do |event|
   event << 'In other words, ***?whereis donut*** will work, but ***?whereis hapy donts*** will not.'
   event << 'If the entered name isn\'t unique, maigo-helper will return a list of suggestions to narrow down your search.'
 end
+
+bot.command(:link, description: 'advice for people with link preview turned off') do |event|
+	event << 'If you don\'t see the google maps link, you should turn your Link Preview on.'
+	event << 'You can find this in User Settings > Text & Images > Link Preview'
+end	
 
 bot.command(:exit, help_available: false) do |event|
   # This is a check that only allows a user with a specific ID to execute this command. Otherwise, everyone would be
