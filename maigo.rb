4ba4c30790b1ddabea7fb2d036bfddd28f4459a8
yuzu-r <tkaneshiro@gmail.com>
2018-06-10 21:11:36 +0000

    using an embed for raid/egg reports

--- maigo.rb
+++ maigo.rb
@@ -1,8 +1,11 @@
 require 'bundler/setup'
 require 'discordrb'
 require_relative 'maigodb'
+require_relative 'helpers'
 require 'chronic'
 require 'tzinfo'
+
+# to do: add toots sweets as an alias
 
 prefix = ENV['DISCORD_PREFIX']
 
@@ -56,132 +59,103 @@ bot.command(:exit, help_available: false) do |event|
   exit
 end
 
-
-## to do: count commas for input, convert egg to the same format input as raid
-##
-
-
 bot.command(:raid, min_args: 1, description: 'report a raid') do |event, *raid_info|
 	server_name = event.server.name
 	channels = bot.find_channel('raids', server_name)
 	raid_channel_id = channels.count > 0 ? channels[0].id : bot.channel.id
-	#puts "raid_info: #{raid_info}"
-	#puts "#{raid_info.join('.')}"
-	parsed_raid_info = raid_info.join('.').gsub(/\./,' ')
-	#puts "parsed_raid_info: #{parsed_raid_info}"
-	parsed_raid_data = parsed_raid_info.split(',') 
-	#p parsed_raid_data
-	parsed_raid_data = parsed_raid_data.map {|s| s.strip}
-	boss, gym, minutes_left = parsed_raid_data
+	username = event.channel.server.member(event.user.id).display_name
 
-	username = event.channel.server.member(event.user.id).nick || event.user.username
-
-=begin	gym_lookup = lookup(gym)
-	if gym_lookup['name']
-		if gym_lookup['name'].downcase != gym.downcase
-			raid_location = gym + ', aka ' + gym_lookup['name']
-		else
-			raid_location = gym_lookup['name']
-		end
-		gmap_link = gym_lookup['gmap'] ? '(<' + gym_lookup['gmap'] + '>)' : ''
+	parsed_raid_data = comma_parse(raid_info)
+	if parsed_raid_data.count != 3
+		error_msg = "Usage: ,raid <boss>,<gym>,<minutes remaining> (separated by commas)"
+		event.respond "<@" + event.user.id.to_s + "> " + error_msg
+		return		
 	else
-		raid_location = gym
+		boss, gym, minutes_left = parsed_raid_data
 	end
-=end
-	tz = TZInfo::Timezone.get('America/Los_Angeles')
 
+	tz = TZInfo::Timezone.get('America/Los_Angeles')
 	despawn_time = tz.utc_to_local(Time.now + minutes_left.to_i*60).strftime("%-I:%M %p")
 
-	bot.send_message(raid_channel_id, "**#{boss.capitalize} raid until #{despawn_time}! (#{minutes_left} mins left)**")
-	bot.send_message(raid_channel_id, "Gym: #{gym} (reported by #{username})")
-	#bot.send_message(raid_channel_id, "reported by: #{event.channel.server.member(event.user.id).nick}")
+	#emoji_name = 'raid_ball'
+	#emoji_mention = get_emoji_mention(emoji_name, event.server.emojis)
+
+	gym_data = lookup(gym)
+	if gym_data['gmap']
+		gym_info = '[' + gym + ']' + '(' + gym_data['gmap'] + ')'
+	else
+		gym_info = gym
+	end
+
+	embed = Discordrb::Webhooks::Embed.new
+	embed.title = "**#{boss.capitalize} raid until #{despawn_time}! (#{minutes_left} mins left)**"
+	embed.color = 15236612
+	embed.description = "Gym: #{gym_info} (reported by #{username})"
+	bot.send_message(raid_channel_id, '',false, embed)
+
+	#bot.send_message(raid_channel_id, "**#{boss.capitalize} raid until #{despawn_time}! (#{minutes_left} mins left)**")
+	#bot.send_message(raid_channel_id, "Gym: #{gym} (reported by #{username})")
 	event.respond "<@" + event.user.id.to_s + "> " + "Your report has been posted to the raids channel! Thanks! "
 
 	true
 	return
 end
 
-CROSS_MARK = "\u274c".freeze
-bot.command(:egg, min_args: 2, description: 'report an egg') do |event, tier, *time|  
+bot.command(:egg, min_args: 1, description: 'report an egg') do |egg_event, *egg_info|  
 	tier_list = [1,2,3,4,5]
+	#tier, gym, minutes_to_hatch = comma_parse(egg_info)
+	parsed_egg_data = comma_parse(egg_info)
+	puts "egg: #{parsed_egg_data}"
+	if parsed_egg_data.count != 3
+		error_msg = "Usage: ,egg <tier>,<gym>,<minutes to hatch> (separated by commas)"
+		egg_event.respond "<@" + egg_event.user.id.to_s + "> " + error_msg
+		return		
+	else
+		tier, gym, minutes_to_hatch = parsed_egg_data
+	end
+
 
 	if tier_list.include?(tier.to_i)
-		time_string = time.length > 0 ? time.join(' ') : nil
-		if time_string
-			parsed_time = Chronic.parse(time_string)
-			if !parsed_time
-				parsed_time = Chronic.parse('this second')
-			end
-		else
-			parsed_time = Chronic.parse('this second')
-		end
-		puts parsed_time
+		time = "in " + minutes_to_hatch + " minutes"
+		parsed_time = Chronic.parse(time)
+
   	# vagrant box is UTC time zone btw
   	tz = TZInfo::Timezone.get('America/Los_Angeles')
 
   	hatch_time = tz.utc_to_local(parsed_time).strftime("%-I:%M %p")	
   	despawn_time = tz.utc_to_local(parsed_time + 45*60).strftime("%-I:%M %p")
 
-		message = event.respond "You are reporting a tier #{tier} egg hatching at #{hatch_time}. Please enter the gym name, or click the X to cancel this report."
-		message.react CROSS_MARK
-		is_message = true
+		username = egg_event.channel.server.member(egg_event.user.id).display_name
+		server_name = egg_event.channel.server.name
+  	channels = bot.find_channel('raids', server_name)
+  	egg_channel_id = channels.count > 0 ? channels[0].id : egg_event.channel.id
+  	# get emoji for egg and its id
+  	case tier.to_i
+  	when 1..2
+  		color = 16724889
+  	when 3..4
+  		color = 13421568
+  	else
+  		color = 8028868
+  	end
+		#emoji_mention = get_emoji_mention(emoji_name, egg_event.server.emojis)
 
-	  event.user.await(:"gym#{event.user.id}") do |gym_event|
-	    # why do I have to do this
-	    if !is_message
-	    	# should i return something to kill the event
-	    	true
-	    else
-	    	gym = gym_event.message.content
-=begin	
-    	gym_lookup = lookup(gym)
-	    	if gym_lookup['name']
-					if gym_lookup['name'].downcase != gym.downcase
-						egg_location = gym + ', aka ' + gym_lookup['name']
-					else
-						egg_location = gym_lookup['name']
-					end
-					gmap_link = gym_lookup['gmap'] ? '(<' + gym_lookup['gmap'] + '>)' : ''
-	    	else
-	    		egg_location = gym
-	    	end
-=end	  
-				username = event.channel.server.member(event.user.id).nick || event.user.username  	
-				server_name = event.channel.server.name
-	    	channels = bot.find_channel('raids', server_name)
-	    	raid_channel_id = channels.count > 0 ? channels[0].id : event.channel.id
-	    	# get emoji for egg and its id
-	    	emoji_name='eggt5'
-	    	emojis = event.server.emojis
-	    	emoji_mention = ''
-	    	emojis.each do |key, e|
-				  #puts "#{e.name}, #{emoji_name}"
-				  if e.name == emoji_name
-				  	emoji_mention = e.mention
-				  	#puts "found it - #{emoji_mention}"
-				  	break
-				  end
-				end
-	    	bot.send_message(raid_channel_id, "#{emoji_mention} **#{tier}* egg hatches #{hatch_time} (despawns #{despawn_time})**")
-	    	bot.send_message(raid_channel_id, "Gym: #{gym} (reported by #{username})")
-	    	gym_event.respond "<@" + event.user.id.to_s + "> " + "Your report has been posted to the raids channel! Thanks! "
-	   		message.delete
-	   		true
-	    end
-	  end
-
-	  bot.add_await(:"delete_#{message.id}", Discordrb::Events::ReactionAddEvent, emoji: CROSS_MARK) do |reaction_event|
-	    # Since this code will run on every CROSS_MARK reaction, it might not
-	    # be on our time message we sent earlier. We use `next` to skip the rest
-	    # of the block unless it was our message that was reacted to.
-	    next true unless reaction_event.message.id == message.id
-	    # Delete the matching message.
-	    message.delete
-	    is_message = false
-	    true
-	  end
+		gym_data = lookup(gym)
+		if gym_data['gmap']
+			gym_info = '[' + gym + ']' + '(' + gym_data['gmap'] + ')'
+		else
+			gym_info = gym
+		end
+  	#bot.send_message(egg_channel_id, "#{emoji_mention} **#{tier}* egg hatches #{hatch_time} (despawns #{despawn_time})**")
+  	#bot.send_message(egg_channel_id, "Gym: #{gym} (reported by #{username})")
+  	embed = Discordrb::Webhooks::Embed.new
+  	embed.title = "**#{tier}* hatches #{hatch_time} (despawns #{despawn_time})**"
+  	embed.color = color
+  	embed.description = "Gym: #{gym_info} (reported by #{username})"
+  	bot.send_message(egg_channel_id, '',false, embed)
+  	egg_event.respond "<@" + egg_event.user.id.to_s + "> " + "Your report has been posted to the raids channel! Thanks! "
 	else
-		event.respond "<@" + event.user.id.to_s + "> " + "please start your report with the egg tier (1-5)"
+		egg_event.respond "<@" + egg_event.user.id.to_s + "> " + "please start your report with the egg tier (1-5)"
 	end
 
 	return
