2c05ecee3dbac2acd602e13a82313636a2efc28a
yuzu-r <tkaneshiro@gmail.com>
2018-06-26 04:44:42 +0000

    exgyms
    exgyms

--- maigo.rb
+++ maigo.rb
@@ -11,6 +11,7 @@ bot = Discordrb::Commands::CommandBot.new token: ENV['DISCORD_TOKEN'],
 usage_text = prefix  + 'whereis [gym name]'
 
 bot.command(:whereis, min_args: 1, description: 'find a PoGo gym', usage: usage_text) do |event, *gym| 
+	username = event.channel.server.member(event.user.id).display_name
 	search_term = gym.join(' ')
 	message = lookup(search_term)
 	if message['name']
@@ -20,23 +21,34 @@ bot.command(:whereis, min_args: 1, description: 'find a PoGo gym', usage: usage_
 			title = message['name']
 		end
 		event << title
-		event << message['address']
-		event << message['landmark']
-		if message['gmap']
-			event << '<' + message['gmap'] + '>'
+		if message['is_ex_eligible']
+			event << 'EX Raid Location!'
 		end
+		event << message['address']
+		if message['landmark']
+			event << 'Near: ' + message['landmark']
+		end
+		# suppress the map preview for brevity
+		google_maps = message['gmap'] ? '<' + message['gmap'] + '>' : nil
+		# trying logging
+		puts "whereis: #{username} successful lookup for #{search_term}"
+		event << google_maps
 	else
 		# either multiple gyms returned, or no gyms found
+		puts "whereis: #{username} not found/unique: #{search_term}"
 		message
 	end
 end
 
 bot.command(:help, description: 'maigo-helper help') do |event|
-  event << 'Type ***?whereis*** and a gym name or nickname to look up its location.'
-  event << 'Try ***?whereis happy donuts*** to see it in action.'
-  event << 'It is not case sensitive. In most cases, it can guess an incomplete name, but not misspelled names.'
-  event << 'In other words, ***?whereis donut*** will work, but ***?whereis hapy donts*** will not.'
-  event << 'If the entered name isn\'t unique, maigo-helper will return a list of suggestions to narrow down your search.'
+	username = event.channel.server.member(event.user.id).display_name
+	puts "help: #{username} asked for help"
+  event << "Type ***#{prefix}whereis*** and a gym name or nickname to look up its location."
+  event << "Try ***#{prefix}whereis happy donuts*** to see it in action."
+  event << "It is not case sensitive. In most cases, it can guess an incomplete name, not typo-ed names."
+  event << "In other words, ***#{prefix}whereis donut*** will work, but ***#{prefix}whereis hapy donts*** will not."
+  event << "If the entered name isn\'t unique, maigo-helper will return a list of suggestions to narrow down your search."
+  event << "\nType ***#{prefix}exgyms*** to see a listing of El Cerrito/Albany gyms known to hold ex raids."
 end
 
 bot.command(:exit, help_available: false) do |event|
@@ -47,6 +59,37 @@ bot.command(:exit, help_available: false) do |event|
   break unless admin_array.include?(event.user.id.to_s)
   bot.send_message(event.channel.id, 'Bot is shutting down, byebye')
   exit
+end
+
+bot.command(:exgyms, description: 'list gyms that are eligible to have ex raids') do |event|
+	username = event.channel.server.member(event.user.id).display_name
+	puts "exgyms: #{username} retrieving ex raid eligible gyms..."
+	ex_gyms = ex_gym_lookup
+	if !ex_gyms || ex_gyms.count == 0
+		bot.send_message(event.channel.id, 'No ex raid gyms found.')
+		return
+	else
+		embed = Discordrb::Webhooks::Embed.new
+		embed.title = "__**Area Gyms Eligible for EX Raids:**__"
+		embed.color = 15236612
+		description = ""
+		ex_gyms.each do |gym|
+			if gym['gmap']
+				gym_info = '[' + gym['name'] + ']' + '(' + gym['gmap'] + ')'
+			else
+				gym_info = gym['name']
+			end
+			description += "\n#{gym_info}"
+		end
+		if description.length > 2048
+			description = description.slice(0,2048)
+		end
+		embed.description = description
+		foot = Discordrb::Webhooks::EmbedFooter.new(text:"Click the gym name for google map.")
+		embed.footer = foot
+		bot.send_message(event.channel.id, '',false, embed)
+	end
+	return
 end
 
 bot.run
