06815de4f7635b17634ba200ebb0c616b826a187
yuzu-r <tkaneshiro@gmail.com>
2019-07-22 23:42:39 -0700

    midnight message - poc

--- src/modules/events/reporting/ready.rb
+++ src/modules/events/reporting/ready.rb
@@ -18,6 +18,10 @@ module Bot::ReportingEvents
 					silent_update(server, _event.bot)	
 				end
 			end
+			midnight_cron = "0 0 * * *"
+			Bot::Scheduler.cron midnight_cron do
+				repeat_post(_event, Bot::MIDNIGHT_POST_CHANNEL_ID, Bot::MIDNIGHT_POST)
+			end
 		end
   end
 end
